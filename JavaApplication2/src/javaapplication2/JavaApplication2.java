9773dc9d3545c9069253db9238ad119b8ff9f03c
Arif Nezami <arifnezami@gmail.com>
2021-01-08 15:58:50 +0600

    Update JavaApplication2.java

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -9,45 +9,112 @@ package javaapplication2;
  *
  * @author arif
  */
+
+//  class A
+//{
+// A(){String s = "Class A";}
+//}
+//class B extends A
+//{
+// String s = "Class B";
+// B (){
+// System.out.println(s);
+// }
+//}
+//class C extends B
+//{
+// String s = "Class C";
+// C (){
+// System.out.println(s);
+// }
+//}
+
+class Super {
+    void print(int a){
+        System.out.println("super: "+ a);
+    }
+    void print(){
+        System.out.println("inside sub");
+    }
+}
+
+class Sub extends Super {
+    void print(int a){
+        System.out.println("super: "+ a);
+    }
+    void print(){
+        System.out.println("inside sub");
+    }
+}
+
+class SubSub extends Sub {
+    void print(int a){
+        System.out.println("super: "+ a);
+    }
+    void print(){
+        System.out.println("inside sub");
+    }
+}
+
 public class JavaApplication2 {
 
     /**
      * @param args the command line arguments
      */
+    
+  
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
-                A superOb = new A();
-                B subOb = new B();
-                superOb.i = 10;
-                superOb.j = 20;
-                superOb.showij();
-                subOb.i = 7;
-                subOb.j = 8;
-                subOb.k = 9;
-                subOb.showij();
-                subOb.showk();
-                subOb.sum();
+          Super x = new Super();
+    Sub y = new Sub();
+    SubSub z = new SubSub();
+  
+    x.print(2);
+    x=y;
+    x.print();
+    y=z;
+    y.print(3);
+    z.print();
+//        C c = new C();
+//    System.out.println(c.s);
+//                A superOb = new A();
+//                B subOb = new B();
+//                superOb.i = 10;
+//                superOb.j = 20;
+//                superOb.showij();
+//                subOb.i = 7;
+//                subOb.j = 8;
+//                subOb.k = 9;
+//                subOb.showij();
+//                superOb.showij();
+//                superOb.i = 7;
+//                superOb.showij();
+//                subOb.showij();
+//               // subOb.showk();
+//               // subOb.sum();
+                
 
         
     }
     
 }
 
-class A {
-int i, j;
-    void showij() {
-    System.out.println(i + " " + j); 
-    }  
- }
+//class A {
+//int i, j;
+//    void showij() {
+//    System.out.println(i + " " + j); 
+//    }  
+// }
+//
+//class B extends A {
+//    int k;
+//    void showk() {
+//        System.out.println(k);
+//    }
+//    void sum() {
+//        System.out.println(i+j+k);
+//        }
+//}
 
-class B extends A {
-    int k;
-    void showk() {
-        System.out.println(k);
-    }
-    void sum() {
-        System.out.println(i+j+k);
-        }
-}
+
