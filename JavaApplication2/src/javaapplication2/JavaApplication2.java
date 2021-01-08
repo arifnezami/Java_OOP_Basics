50dd63b632de33bc585dacf43de92784b84c225b
Arif Nezami <arifnezami@gmail.com>
2021-01-08 15:10:04 +0600

    Update JavaApplication2.java

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -17,22 +17,37 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        B subOb = new B();
-        subOb.k = 10;
-        subOb.showk();
+        
+                A superOb = new A();
+                B subOb = new B();
+                superOb.i = 10;
+                superOb.j = 20;
+                superOb.showij();
+                subOb.i = 7;
+                subOb.j = 8;
+                subOb.k = 9;
+                subOb.showij();
+                subOb.showk();
+                subOb.sum();
+
         
     }
     
 }
 
 class A {
-int i, j;    
-}
+int i, j;
+    void showij() {
+    System.out.println(i + " " + j); 
+    }  
+ }
 
 class B extends A {
     int k;
     void showk() {
         System.out.println(k);
     }
-    
+    void sum() {
+        System.out.println(i+j+k);
+        }
 }
